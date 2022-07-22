5fb43f8fa1339819b5922ea29ee105f715867db4
greene5952 <dustin@wolf-solutions.us>
2022-07-22 08:00:09 -0500

    update

--- scripts/deployArgs.js
+++ scripts/deployArgs.js
@@ -1,11 +1,11 @@
 module.exports = [
- 'Sloth Club NFT NFT',
- 'SCNFT',
+ 'DegenCandy NFT',
+ 'DCNFT',
  
     '0x9C3f261e2cc4C88DfaC56A5B46cdbf767eE2f231',
     '0xc01aC67ba14F13Af4fCD5b4ba3784F39a703cda0',
-    'https://public-pre-ipfs.s3.amazonaws.com/Sloth_Club_NFT/assets/SlothReveal.json',
-    'https://public-pre-ipfs.s3.amazonaws.com/Sloth_Club_NFT/json/',
+    'https://public-pre-ipfs.s3.amazonaws.com/Degen_Candy_NFT/json/',
+    'https://public-pre-ipfs.s3.amazonaws.com/Degen_Candy_NFT/assets/DCReveal.json',
 
     [
         ethers.utils.getAddress('0x0535a5Bc294B5b5De4b30A542DF155061EC3346D'),
